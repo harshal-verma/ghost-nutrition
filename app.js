57f04beaa8e162ba496ab5a262b1781e6aa5d925
harshal-verma <81582281+harshal-verma@users.noreply.github.com>
2021-06-06 22:45:43 +0530

    updated app.js

--- app.js
+++ app.js
@@ -121,13 +121,33 @@ const productsArray = [
     }
     
 ]
-
+const productEl = document.querySelector(".products__flex");
 function showProducts(array){
     array.forEach((product) => {
-        const productEl = document.createElement(div);
-        productEl.innerHTML = `
-                
-        `
+
+       const productCardEl = document.createElement('div');
+       productCardEl.innerHTML = `
+       <div class="products__card">
+       <figure><img src=${product.image} alt=${product.name} class="products__image"></figure>
+       <h3 class="product__heading">${product.name}</h3>
+       <p class="product__description"><span class="description__bold">Description: </span>${product.description}</p>
+       <div class="products__pq">
+           <div class="products__price">
+               <p>Price</p>
+               <h2>${product.price}$</h2>
+           </div>
+           <div class="products__quatities">
+               <p>Quantity</p>
+               <i class="fas fa-less-than"></i> <span>1</span> <i class="fas fa-greater-than"></i>
+           </div>
+       </div>
+       <button class="add__to__cart">Add to cart</button><button class="view__product">View 
+            <i class="fas fa-greater-than"></i>
+       </button>
+   </div>
+       `;
+       productEl.append(productCardEl);
     })
 }
 
+showProducts(productsArray);
