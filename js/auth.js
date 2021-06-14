2e8af6274b4fc30fba40cf7a2ed0f809f1be70e7
harshal-verma <81582281+harshal-verma@users.noreply.github.com>
2021-06-14 08:54:56 +0530

    updated auth.js and index.js

--- js/auth.js
+++ js/auth.js
@@ -1,3 +1,44 @@
+//get data
+// db.collection('products').get().then(snapshot => {
+//     console.log(snapshot.docs)
+// ;});
+const setupGuides = (data) => {
+    if(data.length){
+    // setupGuides(snapshot.docs)
+    let html = '';
+    data.forEach(doc => {
+        const guide = doc.data();
+        // console.log(guide);
+        const li = `
+           <li>
+              <div>${guide}</div>
+              <div>${guide.content}</div>
+           </li>
+        `;
+        html += li;
+    });
+    guideList.innerHTML = html;
+  }else{
+    guideList.innerHTML = '<h5>Login to view guides</h5>'
+  }
+}
+
+//listen for auth status changes 
+auth.onAuthStateChanged(user => {
+    // console.log(user);
+    if(user){
+        console.log("user logged in:", user);
+        db.collection('guides').get().then(snapshot => {
+            setupGuides(snapshot.docs);
+            setupUI(user);
+        });
+    }else{
+        console.log("user is logged out")
+        setupUI();
+        setupGuides([]);
+    }
+})
+
 //sign up
 const signupForm = document.querySelector("#signup-form");
 signupForm.addEventListener("submit", (e) => {
@@ -5,7 +46,6 @@ signupForm.addEventListener("submit", (e) => {
     //get user info
     const email = signupForm['signup-email'].value;
     const password = signupForm['signup-password'].value;
-    console.log(email , password);
 
     //sign up the user
     auth.createUserWithEmailAndPassword(email,password).then(cred => {
@@ -20,9 +60,23 @@ const logout = document.querySelector("#logout");
 logout.addEventListener("click" ,(e) => {
     e.preventDefault();
     auth.signOut().then(() => {
-        console.log("user has signed out !!!");
     });
 })
 
 //login in method
-const login = document.querySelector("")
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    //get user info
+    const email = loginForm["login-email"].value;
+    const password = loginForm["login-password"].value;
+
+    auth.signInWithEmailAndPassword(email, password).then((cred) => {
+        //close the login modal and reset the form
+        const modal = document.querySelector("#modal-login");
+        loginForm.reset();
+    })
+})
+
+//log ou
