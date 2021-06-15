2684d2cac9dfe2c2b30e7e808ebbdf78645b8e99
harshal-verma <81582281+harshal-verma@users.noreply.github.com>
2021-06-15 14:34:51 +0530

    added cancel btn

--- js/auth.js
+++ js/auth.js
@@ -1,31 +1,5 @@
-//get data
-// db.collection('products').get().then(snapshot => {
-//     console.log(snapshot.docs)
-// ;});
-const setupGuides = (data) => {
-    if(data.length){
-    // setupGuides(snapshot.docs)
-    let html = '';
-    data.forEach(doc => {
-        const guide = doc.data();
-        // console.log(guide);
-        const li = `
-           <li>
-              <div>${guide}</div>
-              <div>${guide.content}</div>
-           </li>
-        `;
-        html += li;
-    });
-    guideList.innerHTML = html;
-  }else{
-    guideList.innerHTML = '<h5>Login to view guides</h5>'
-  }
-}
 const accountDetails = document.querySelector("#account-modal");
-//listen for auth status changes 
 auth.onAuthStateChanged(user => {
-    // console.log(user);
     if(user){
         console.log("user logged in as: ", user.email);
         const html = `
@@ -33,25 +7,17 @@ auth.onAuthStateChanged(user => {
         `
         accountDetails.innerHTML = html;
         db.collection('guides').get().then(snapshot => {
-            setupGuides(snapshot.docs);
-            setupUI(user);
         });
     }else{
         console.log("user is logged out")
-        setupUI();
-        setupGuides([]);
     }
 })
 
-//sign up
 const signupForm = document.querySelector("#signup-form");
 signupForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    //get user info
     const email = signupForm['signup-email'].value;
     const password = signupForm['signup-password'].value;
-
-    //sign up the user
     auth.createUserWithEmailAndPassword(email,password).then(cred => {
         const modal = document.querySelector("#signup-modal");
         modal.close();
@@ -59,7 +25,6 @@ signupForm.addEventListener("submit", (e) => {
     })
 })
 
-//log out method 
 const logout = document.querySelector("#logout");
 logout.addEventListener("click" ,(e) => {
     e.preventDefault();
@@ -67,20 +32,14 @@ logout.addEventListener("click" ,(e) => {
     });
 })
 
-//login in method
 const loginForm = document.querySelector("#login-form");
 loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    
-    //get user info
     const email = loginForm["login-email"].value;
     const password = loginForm["login-password"].value;
 
     auth.signInWithEmailAndPassword(email, password).then((cred) => {
-        //close the login modal and reset the form
         const modal = document.querySelector("#modal-login");
         loginForm.reset();
     })
 })
-
-//log ou
--- js/index.js
+++ js/index.js
@@ -1,36 +1,22 @@
 const guidesList = document.querySelector(".guides");
 const loggedOutLinks = document.querySelectorAll(".logged-out");
 const loggedInLinks = document.querySelectorAll(".logged-in");
+const cancelBtn = document.querySelectorAll(".fa-times-circle");
+const modalCard = document.querySelectorAll(".modal");
 
-const setupUI = (user) => {
-    if(user){
-        //toggle UI elements
-        loggedInLinks.forEach(item => item.style.display = 'block');
-        loggedOutLinks.forEach(item => item.style.display = 'none');
-    }else{
-        //toggle UI elements
-        loggedInLinks.forEach(item => item.style.display = 'none');
-        loggedOutLinks.forEach(item => item.style.display = 'block');
-    }
-}
+// const setupUI = (user) => {
+//     if(user){
+//         //toggle UI elements
+//         loggedInLinks.forEach(item => item.style.display = 'block');
+//         loggedOutLinks.forEach(item => item.style.display = 'none');
+//     }else{
+//         //toggle UI elements
+//         loggedInLinks.forEach(item => item.style.display = 'none');
+//         loggedOutLinks.forEach(item => item.style.display = 'block');
+//     }
+// }
 
-const setupGuides = (data) => {
-    if(data.length){
-    // setupGuides(snapshot.docs)
-    let html = '';
-    data.forEach(doc => {
-        const guide = doc.data();
-        // console.log(guide);
-        const li = `
-           <li>
-              <div>${guide}</div>
-              <div>${guide.content}</div>
-           </li>
-        `;
-        html += li;
-    });
-    guideList.innerHTML = html;
-  }else{
-    guideList.innerHTML = '<h5>Login to view guides</h5>'
-  }
+function cancelModal(){
+    document.querySelector(".fa-times-circle").style.display = 'none';
+    console.log("dkvdkd")
 }
